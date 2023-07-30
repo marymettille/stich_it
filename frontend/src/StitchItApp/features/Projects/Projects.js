23487ae897c1d5679ac0b387acb50d6d14946c03
marymettille <90646899+marymettille@users.noreply.github.com>
2023-07-30 15:59:42 -0500

    Change Filing Structure of frontend

--- frontend/src/StitchItApp/features/Projects/Projects.js
+++ frontend/src/StitchItApp/features/Projects/Projects.js
@@ -8,5 +8,9 @@ export const Projects = () => {
     });
   });
 
-  return <p>Projects will go here.</p>;
+  return (
+    <>
+      <p>Projects will go here.</p>
+    </>
+  );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { StitchItApp } from "./App/StitchItApp";
+import { StitchItApp } from "./StitchItApp/App/StitchItApp";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
