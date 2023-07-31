29645fb3bc00e83471ccb43ad82bb764fbf213b3
marymettille <90646899+marymettille@users.noreply.github.com>
2023-07-30 22:09:13 -0500

    Create the cutest of little tiny loading spinners

---
+++ frontend/src/design-system/Loading/Loading.stories.js
@@ -0,0 +1,11 @@
+import { Loading } from "./Loading";
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  title: "Loading",
+  component: Loading,
+};
+
+// const Template = (props) => <Button {...props} />;
+
+export const Default = {};
