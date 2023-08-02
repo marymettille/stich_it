b36dfb8ee016626d3d4ea1f519af245cc0b2ac42
marymettille <90646899+marymettille@users.noreply.github.com>
2023-08-02 18:24:24 -0500

    Button should refuse to reload the page and automatically run onClick function if enter is pressed

--- frontend/src/design-system/Button/Button.js
+++ frontend/src/design-system/Button/Button.js
@@ -1,5 +1,25 @@
 import "./style.scss";
 
-export const Button = ({ children }) => {
-  return <button className="base-button">{children}</button>;
+export const Button = ({ children, onClick }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    onClick();
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onClick();
+    }
+  };
+
+  return (
+    <button
+      className="base-button"
+      onClick={handleClick}
+      onKeyDown={handleKeyDown}
+      aria-label={children}
+    >
+      {children}
+    </button>
+  );
 };
