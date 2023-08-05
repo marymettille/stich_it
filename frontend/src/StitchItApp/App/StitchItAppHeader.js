e68864ee804011f0a72f63d9d1696c5633e3664f
marymettille <90646899+marymettille@users.noreply.github.com>
2023-08-05 15:21:06 -0500

    Add naviagation buttons to the header

--- frontend/src/StitchItApp/App/StitchItAppHeader.js
+++ frontend/src/StitchItApp/App/StitchItAppHeader.js
@@ -1,9 +1,17 @@
+import { Button } from "../../design-system";
+import { useNavigate } from "react-router-dom";
 import "./StitchItAppHeader.scss";
 
 export const StitchItAppHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="stitch-it-app-header">
       <h2>Time to Get Stitching!</h2>
+      <div className="navigation-controls">
+        <Button icon="home" onClick={() => navigate(`/`)} />
+        <Button icon="new" onClick={() => navigate(`/pattern/create`)} />
+      </div>
     </header>
   );
 };
