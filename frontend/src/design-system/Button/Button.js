febeef013b0703beeb72d7bd174b1dfa33115ce6
marymettille <90646899+marymettille@users.noreply.github.com>
2023-08-05 11:24:35 -0500

    Add ClassNames to design-system

--- frontend/src/design-system/Button/Button.js
+++ frontend/src/design-system/Button/Button.js
@@ -1,6 +1,8 @@
+import classNames from "classnames";
 import "./style.scss";
 
-export const Button = ({ children, onClick }) => {
+export const Button = ({ children, className, onClick }) => {
+  const classes = classNames("base-button", className);
   const handleClick = (e) => {
     e.preventDefault();
     onClick();
@@ -14,7 +16,7 @@ export const Button = ({ children, onClick }) => {
 
   return (
     <button
-      className="base-button"
+      className={classes}
       onClick={handleClick}
       onKeyDown={handleKeyDown}
       aria-label={children}
--- frontend/src/design-system/Input/Input.js
+++ frontend/src/design-system/Input/Input.js
@@ -1,10 +1,19 @@
+import classNames from "classnames";
 import "./style.scss";
 
-export const Input = ({ labelText, placeholderText, name, onChange }) => {
+export const Input = ({
+  className,
+  labelText,
+  placeholderText,
+  name,
+  onChange,
+}) => {
+  const classes = classNames("base-input", className);
   return (
-    <div className="base-input">
+    <div className={classes}>
       {labelText && <label htmlFor={name}>{labelText}</label>}
       <input
+        type="text"
         id={name}
         aria-label={placeholderText}
         placeholder={placeholderText}
