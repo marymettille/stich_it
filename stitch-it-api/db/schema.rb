1f6c04978006a9cdde4bf2090653eb9a0040e99f
marymettille <90646899+marymettille@users.noreply.github.com>
2023-11-07 14:09:53 -0600

    Update schema.rb with craft table

--- stitch-it-api/db/schema.rb
+++ stitch-it-api/db/schema.rb
@@ -10,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_30_212040) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_05_203013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "crafts", id: :serial, force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "patterns", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "author"
     t.text "description"
+    t.integer "craft_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
